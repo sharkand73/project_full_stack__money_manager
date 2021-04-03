7d4a57089efd0a1ac65e267d73c6d96cd8a68979
Andrew Sharkey <sharkand@hotmail.com>
2021-04-03 17:54:36 +0100

    Transactions routes half way there.

--- money_manager/controllers/transaction_controller.py
+++ money_manager/controllers/transaction_controller.py
@@ -6,3 +6,16 @@ import repositories.merchant_repository as merchant_repo
 import repositories.transaction_repository as transaction_repo
 
 transactions_blueprint = Blueprint("transactions", __name__)
+
+@transactions_blueprint.route("/transactions")
+def transactions():
+    transaction_list = transaction_repo.select_all()
+    return render_template("/transactions/index.html", transactions = transaction_list, title = "View Transactions")
+
+@transactions_blueprint.route("/transactions/<id>/delete", methods=["POST"])
+def delete(id):
+    transaction = transaction_repo.find_by_id(id)
+    if len(transaction_repo.select_all()) > 1:
+        transaction_repo.delete(transaction)
+    return redirect("/transactions")
+
--- money_manager/repositories/transaction_repository.py
+++ money_manager/repositories/transaction_repository.py
@@ -17,7 +17,7 @@ def select_all():
     transactions = []
     for row in results:
         merchant = merchant_repo.find_by_id(row['merchant_id'])
-        amount = row('amount')
+        amount = row['amount']
         tag = tag_repo.find_by_id(row['tag_id'])
         id = row['id']
         transaction = Transaction(merchant, amount, tag, id)
@@ -30,7 +30,7 @@ def find_by_id(id):
     result = run_sql(sql, values)[0]
     if result != None:
         merchant = merchant_repo.find_by_id(result['merchant_id'])
-        amount = result('amount')
+        amount = result['amount']
         tag = tag_repo.find_by_id(result['tag_id'])
         transaction = Transaction(merchant, amount, tag, id)
         return transaction
@@ -39,3 +39,12 @@ def delete_all():
     sql = "DELETE FROM transactions"
     run_sql(sql)
 
+def delete(transaction):
+    sql = "DELETE FROM transactions WHERE id=%s"
+    values = [transaction.id]
+    run_sql(sql, values)
+
+def update(transaction):
+    sql = "UPDATE transactions SET ( merchant_id, amount, tag_id ) = ( %s,%s,%s ) WHERE id=%s"
+    values = [transaction.merchant.id, transaction.amount, transaction.tag.id, transaction.id]
+    run_sql(sql, values)
