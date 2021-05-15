8a05759e5a39d0be5f56c0a0c86e71dfb0c8b5d5
Andrew Sharkey <sharkand@hotmail.com>
2021-05-15 22:48:53 +0100

    Commented out mm_alt.sql file

--- money_manager/db/mm_alt.sql
+++ money_manager/db/mm_alt.sql
@@ -1,28 +1,34 @@
-DROP TABLE transactions;
-DROP TABLE merchants;
-DROP TABLE tags;
-DROP TABLE budgets;
-DROP TABLE testimonials;
-
-CREATE TABLE tags as TABLE tags_backup;
-
-CREATE TABLE merchants as TABLE merchants_backup;
-
-CREATE TABLE transactions as TABLE transactions_backup;
-
-CREATE TABLE budgets as TABLE budgets_backup;
-
-CREATE TABLE testimonials (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    quote VARCHAR(255)
-);
+-- This sql file was created in order to restore 
+-- backup transactions during a phase in which I was
+-- doing a lot of deleting of transactions, but also 
+-- needed to have a month's worth of transactions handy.
 
 
-INSERT INTO testimonials (name, quote) VALUES ('Boaby, pure mad wi'' it, Ruchill', 'It''s bin pure amazin'' so it hus, a''ve definetly got mair money fur Buckie on a Wensday since a''ve did ma budget.'); 
-INSERT INTO testimonials (name, quote) VALUES ('Ashraf, proprietor of Bammy Beverages', 'Sales are through the roof since my clients started using the app.');
-INSERT INTO testimonials (name, quote) VALUES ('devShark''s mum', 'Whoever coded this deserves the Nobel Peace Prize');
-INSERT INTO testimonials (name, quote) VALUES ('J. Caesar', 'Magnum opus!');
-INSERT INTO testimonials (name, quote) VALUES ('Donald J. Trump', ' I don''t know this guy devShark, I never met him.');
-INSERT INTO testimonials (name, quote) VALUES ('B. Gates', 'This kid has no future in tech.');
-INSERT INTO testimonials (name, quote) VALUES ('S. Cowell', 'Worst CSS I''ve ever seen.');
+-- DROP TABLE transactions;
+-- DROP TABLE merchants;
+-- DROP TABLE tags;
+-- DROP TABLE budgets;
+-- DROP TABLE testimonials;
+
+-- CREATE TABLE tags as TABLE tags_backup;
+
+-- CREATE TABLE merchants as TABLE merchants_backup;
+
+-- CREATE TABLE transactions as TABLE transactions_backup;
+
+-- CREATE TABLE budgets as TABLE budgets_backup;
+
+-- CREATE TABLE testimonials (
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(255),
+--     quote VARCHAR(255)
+-- );
+
+
+-- INSERT INTO testimonials (name, quote) VALUES ('Boaby, pure mad wi'' it, Ruchill', 'It''s bin pure amazin'' so it hus, a''ve definetly got mair money fur Buckie on a Wensday since a''ve did ma budget.'); 
+-- INSERT INTO testimonials (name, quote) VALUES ('Ashraf, proprietor of Bammy Beverages', 'Sales are through the roof since my clients started using the app.');
+-- INSERT INTO testimonials (name, quote) VALUES ('devShark''s mum', 'Whoever coded this deserves the Nobel Peace Prize');
+-- INSERT INTO testimonials (name, quote) VALUES ('J. Caesar', 'Magnum opus!');
+-- INSERT INTO testimonials (name, quote) VALUES ('Donald J. Trump', ' I don''t know this guy devShark, I never met him.');
+-- INSERT INTO testimonials (name, quote) VALUES ('B. Gates', 'This kid has no future in tech.');
+-- INSERT INTO testimonials (name, quote) VALUES ('S. Cowell', 'Worst CSS I''ve ever seen.');
